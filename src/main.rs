0fd46a261c85a075ecf12bc671ef2328addfa114
charlieolinsky <charlesolinsky@gmail.com>
2023-02-10 21:34:59 -0500

    finished tutorial 4
    -added compound data types
    -learned more about changing data types on existing variables.

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,20 @@ fn main() {
         println!("x is: {}", x);
 
         //TYPE CHANGE
+            //We can overwrite existing variables with a new type: 
             let x = "Hello!"; 
             println!("x is: {}", x);
+
+            //Notice that a variable set to another variable will 
+            //retain the original variables type
+            //and not default to i32:  
+            let z: u8 = 5; 
+            let y = z; 
 
         //CONSTANTS
             //must be written in capital snake case, and have a defined type. 
             const SECONDS_IN_MINUTE: u32 = 60;
             println!("constant is: {}", SECONDS_IN_MINUTE);
-
 
 
     //SCALAR DATA TYPES
@@ -36,30 +42,66 @@ fn main() {
             //NOTE: range of values, k, is a funtion of the # of bits, n 
 
             //Signed Integer
-            let k: i8; 
+            let _k: i8; 
             // k = [(-2^7), (2^7)-1] = [-128, 127]
 
             
             //Unsigned Integer
-            let k: i8; 
+            let _k: i8; 
             // k  =  [0, (2^n)-1]  =>  [0, (2^8)-1]  =  [0, 255]
 
         //FLOATING-POINT
             //Floating point w bit specifications.
             // i32  i64
 
-            let fp: f32 = 10.8; //Single-precision
-            let fp: f64 = 10.8; //Double-precision
+            let _fp: f32 = 10.8; //Single-precision
+            let _fp: f64 = 10.8; //Double-precision
 
         //BOOLEAN
-            let f: bool = false; //0
-            let f: bool = true;  //1
+            let _f: bool = false; //0
+            let _f: bool = true;  //1
 
         //CHARACTER
-            let letter: char = 'a';
+            let _letter: char = 'a';
 
             
     //COMPOUND TYPES
+
+            //TUPLE
+                //you can place any data types in here
+                let mut tup: (i32, bool, char) = (1, true, 's'); 
+
+                //PRINTING
+                    //println!("{}", tup);
+                    //^ Error: must reference by index
+                    println!("Tuple: {}", tup.0); 
+
+                //MUTATION (must include mut) 
+                    tup.0 = 10; 
+                    println!("New Tuple: ({}, {}, {})", tup.0, tup.1, tup.2);
+
+                    //you can actually mutate the whole thing:
+                    tup = (3, false, 'a'); 
+                    println!("New Tuple: ({}, {}, {})", tup.0, tup.1, tup.2);
+
+                //NOTE: You can not change the size of a tuple without reintializing it. 
+                //tup = (3, false, 'a', 200); 
+                //^ERROR
+
+            //ARRAY
+                let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
+
+                //Arrays, unlike tuples, must contain all of the same type 
+                //of element. No mismatching types. 
+
+                //PRINTING
+                    //println!("{}", arr);
+                    //^ Error: must reference by index
+                    println!("New Array: [{}, {}, {} ...]", arr[0], arr[1], arr[2]);
+
+                //NOTE: Arrays have no default values like 0. 
+                //An array declared with no values will be empty. 
+
 
 
 }
